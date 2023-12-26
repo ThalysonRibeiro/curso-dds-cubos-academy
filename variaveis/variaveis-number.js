e8c9be7e918aa554e9c1b99210efa737fa283041
Rafinha <rafinha.head@gmail.com>
2023-12-25 22:33:49 -0400

    arquivo criado e comentados

---
+++ variaveis/variaveis-number.js
@@ -0,0 +1,14 @@
+//variaveis sempre começar com letras menusculas
+//usar modelo camelcase, a cada converção de palavra usar letra maiuscula, ex:intenMental
+//texte em programação se chama de string
+//numeros é dados number
+//pode ser guardado numeros decimais ou numeros interiros
+{
+    let idade = 31;
+    console.log(idade);
+}
+
+{
+    let alturaEmMetros = 1.72;
+    console.log(alturaEmMetros);
+}
