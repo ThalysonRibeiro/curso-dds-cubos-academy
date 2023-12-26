461bbf9ccfc3783122248e793ba5563d6035b417
Rafinha <rafinha.head@gmail.com>
2023-12-26 00:51:18 -0400

    arquivo criado e comentado

---
+++ variaveis/variaveis-modulo.js
@@ -0,0 +1,5 @@
+let resto = 12 % 5; // isso é o resto da divisaõ de 12 dividido 5
+//o resulsato de 12/5 é 2. o resto é 2
+
+console.log(resto);
+//é assim que se calcula o esto de uma divisaõ em js
