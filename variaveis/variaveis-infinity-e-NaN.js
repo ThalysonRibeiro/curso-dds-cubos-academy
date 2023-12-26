1d0a31f5f065bad92a238d07e03eb2b0c0816749
Rafinha <rafinha.head@gmail.com>
2023-12-26 00:39:14 -0400

    arquivo criado e comentado

---
+++ variaveis/variaveis-infinity-e-NaN.js
@@ -0,0 +1,14 @@
+NaN
+Infinity
+//Equação de primeiro grau
+// x=0/2  R=0
+// x=2/0 R=inexistente
+// 0/0 R=indeterminado
+let x = 0 / 2;
+console.log(x);// 0
+
+x = 2 / 0;
+console.log(x); // Infinity
+
+x = 0 / 0;
+console.log(x); // NaN
