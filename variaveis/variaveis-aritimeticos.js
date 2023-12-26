14553229aae078758140b289f00a5fc3942b6a8f
Rafinha <rafinha.head@gmail.com>
2023-12-25 23:51:33 -0400

    arquivo criado   comentado

---
+++ variaveis/variaveis-aritimeticos.js
@@ -0,0 +1,14 @@
+let x = 10, y = 2;
+
+let resultado = x + y;
+
+console.log(resultado);//=12
+
+resultado = x - y;
+console.log(resultado);//=18
+
+resultado = x * y;
+console.log(resultado);//=20
+
+resultado = x / y;
+console.log(resultado);//=5
