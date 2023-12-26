b2eb97e525193dfe792608109bd4d7ec0333f11d
Rafinha <rafinha.head@gmail.com>
2023-12-25 22:38:24 -0400

    arquivo criado e comentados

---
+++ variaveis/const-var-let.js
@@ -0,0 +1,8 @@
+//diferenças entre var let e const
+//var no js puxa pro começo do codigo
+//let pode ser feita varias atribuições
+//const só pode ter uma atribuição
+var mestre = 123;
+let texto = 1;
+let texte = 2
+const teste = 31;
