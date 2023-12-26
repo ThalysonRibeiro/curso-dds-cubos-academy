6b1c6f99326862583958b958fc3223a727887c01
Rafinha <rafinha.head@gmail.com>
2023-12-26 01:01:43 -0400

    arquivo criado e comentado

---
+++ variaveis/variaveis-x++.js
@@ -0,0 +1,12 @@
+//exemplo
+// x++ e ++x
+let x = 10;
+//x = x = 1;
+//x+= 1;
+
+x++;//incremente mais um ao proprio x
+x--;
+let y = ++x;// orimeiro soma depois retorna
+//let y = x++;// primeiro retorna depois soma
+console.log("x: ", x);
+console.log("y: ", xy);
