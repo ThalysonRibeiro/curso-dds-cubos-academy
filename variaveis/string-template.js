ed4f64b640c4cdb73998b8688aed0a5dfdb47f1f
Rafinha <rafinha.head@gmail.com>
2023-12-26 00:21:48 -0400

    arquivo criado e comentado

---
+++ variaveis/string-template.js
@@ -0,0 +1,11 @@
+
+let idade = 31;
+console.log(idade);
+
+//camelcase
+let alturaEmMetros = 1.72;
+//console.log(alturaEmMetros);
+
+//string template, é obrigatorio usar crase ``
+// `text ${} text ${}`
+console.log(`Mnha idaede é ${idade} e minha altuara é ${alturaEmMetros}`);
