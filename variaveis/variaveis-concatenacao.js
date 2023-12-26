f0fa6ba0ec41c9531769055baeb97d3f475ae6d0
Rafinha <rafinha.head@gmail.com>
2023-12-26 01:06:16 -0400

    arquivo criado e comentado

---
+++ variaveis/variaveis-concatenacao.js
@@ -0,0 +1,9 @@
+// concatenar é juntar
+
+let nome = "Thalyson";
+let sobreNome = "Rafael";
+
+let nomeCompleto = nome + " " + sobreNome;
+
+console.log(nomeCompleto);
+console.log(`${nome} ${sobreNome}`);
