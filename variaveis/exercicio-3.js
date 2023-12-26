821474cd2f17e7825bb21a22d2a76f7aaad81b49
Rafinha <rafinha.head@gmail.com>
2023-12-26 00:13:58 -0400

    exercicio-3 resolvido

---
+++ variaveis/exercicio-3.js
@@ -0,0 +1,17 @@
+let x = 10, y = 2;
+
+let resultado = x + y;
+let mensagem = "A soma de " + x + " e " + y + " é " + resultado;
+console.log(mensagem); //A soma de 10 e 2 é 12
+
+resultado = x - y; // A subtração de 10 e 2 é 8
+mensagem = "A subtração de " + x + " e " + y + " é " + resultado;
+console.log(mensagem);
+
+resultado = x * y; // A multiplicação de 10 e 2 é 20
+mensagem = "A multiplicação de " + x + " e " + y + " é " + resultado;
+console.log(mensagem);
+
+resultado = x / y; // A divisão de 10 e 2 é 5
+mensagem = "A divisão de " + x + " e " + y + " é " + resultado;
+console.log(mensagem);
