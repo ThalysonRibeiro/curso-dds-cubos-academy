631ab447642f3a5c6a26135d6c2c1a6578c00482
Rafinha <rafinha.head@gmail.com>
2023-12-25 23:13:51 -0400

    arquivo criado   comentado

---
+++ variaveis/tipos-de-dados.js
@@ -0,0 +1,20 @@
+//boolean = verdadeiro ou falso
+//string = textos
+//number = numeros
+//null = valor nulo
+//underfined = valor não definido
+//symbol =
+//object = 
+
+let nome = "Thalyson"; //string
+let idade = 31; //number
+let altura = 1.72; //number
+
+let nota; //underfinednota 
+console.log(nota);
+
+nota = 10;
+console.log(nota);
+
+nota = null;
+console.log(nota);
