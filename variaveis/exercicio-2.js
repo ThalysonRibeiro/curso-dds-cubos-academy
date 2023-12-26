a7eb4700d9d49d5941a098e70f0d0b427666544f
Rafinha <rafinha.head@gmail.com>
2023-12-25 22:51:59 -0400

    exercicio resolvido e  comentado

---
+++ variaveis/exercicio-2.js
@@ -0,0 +1,11 @@
+//declarar uma variavel mutavel.
+// guardar na variavel o numero da populção da cidade onde mora.
+//imprimir o numero guardado na variavel.
+//depois modificar e imprimir novamente.
+let populaçãoAtualDaCidade = 890.480;
+
+console.log(populaçãoAtualDaCidade);
+
+populaçãoAtualDaCidade = 899.510;
+
+console.log(populaçãoAtualDaCidade);
