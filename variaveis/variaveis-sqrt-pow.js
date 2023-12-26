e7dedf82a5fb2f1e97f16be45770fa30a73babd1
Rafinha <rafinha.head@gmail.com>
2023-12-26 01:14:29 -0400

    arquivo criado e comentado

---
+++ variaveis/variaveis-sqrt-pow.js
@@ -0,0 +1,8 @@
+let raiz = Math.sqrt(9);
+//Math.sqrt(x: number): number
+console.log(raiz);
+
+let potencia = Math.pow(2, 3);// 2 elevado a tercera potencia
+// outra forma de se fazdr potencia
+potencia = 2 ** 3;
+console.log(potencia);
