102e3d6c6c35f80a20cfc371015380f99433b8b4
Rafinha <rafinha.head@gmail.com>
2023-12-25 22:44:59 -0400

    exercicio resolvido e  comentados

---
+++ variaveis/exercicio-1.js
@@ -0,0 +1,5 @@
+//criar variavel imutavel.
+//guardar na variavel o nome da cidade natal.
+//imprimir o nome da cidade natal, imprimir o conteudo da variavel.
+const cidadeNatal = "Natal-RN";
+console.log(cidadeNatal);
