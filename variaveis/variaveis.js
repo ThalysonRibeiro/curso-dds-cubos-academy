f5e33b94ecd919ef84c328492f2f2d1f4be1a950
Rafinha <rafinha.head@gmail.com>
2023-12-25 22:21:22 -0400

    cometarios adicionais

--- variaveis/variaveis.js
+++ variaveis/variaveis.js
@@ -1,5 +1,15 @@
 //criando variavel const
 // const é uma variavel constante. é uma variavel que nãp pode mais ser mudada.
 //não se pode fazer uma reatribuição a variavel const.
-const primeiroNome = "Thalyson";
+{
+    const primeiroNome = "Thalyson";
 console.log(primeiroNome);
+}
+
+{
+//fazer uma reatribuição a mesma variavel.
+//variavel let pode reatribuir outro valor a mesma variavel. ex:
+let primeiroNome = "Thalyson";
+primeiroNome = "jhonsonn";
+console.log(primeiroNome);
+}
