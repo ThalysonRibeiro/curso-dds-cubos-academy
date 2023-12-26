9a191c6fc87a568b86028ff770d7c093d8d47918
Rafinha <rafinha.head@gmail.com>
2023-12-26 00:45:46 -0400

    arquivo criado e comentado

--- variaveis/variaveis-tipos-corretos.js
+++ variaveis/variaveis-tipos-corretos.js
@@ -1,7 +1,7 @@
 //let x = 10, y = 2; normal
 //let x = 10, y = "2"; concatena = 102
 //let x = "10", y = 2; concatena = 102
-let x = "10", y = "2";
+//let x = "10", y = "2"; concatena = 102
 
 let resultado = x + y;
 console.log(`A soma de ${x} e ${y} é ${resultado}`);// A soma de 10 e 2 é 12
