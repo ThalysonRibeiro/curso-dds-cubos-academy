0c80fb4228c13f344630781641cde60e362a0a61
Rafinha <rafinha.head@gmail.com>
2024-01-02 23:53:19 -0400

    pequena correção

--- array-loops/exercicio-3.js
+++ array-loops/exercicio-3.js
@@ -3,7 +3,7 @@ const numeros = [22, 67, 2, 58, 47, 6, 7, 8]; // tamho 8 | indice 7
 
 let soma = 0;
 for (let i = 0; i < numeros.length; i++) {
-    //sera executado para i de 0 até 7 | o não sera mais executado
+    //sera executado para i de 0 até 7 | 0 não sera mais executado
     soma = soma + numeros[i];
     //soma += numeros[i];
 }
